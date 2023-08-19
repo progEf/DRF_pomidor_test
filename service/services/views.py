4d975ea605defeafa6ca0f0bb4eae2216ce25a90
bot_tel <qweasd12401u@gmail.com>
2023-08-19 14:06:29 +0300

    day 2-2

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from services.models import Subscription
+from services.serializers import SubscriptionSerializers
+
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class  = SubscriptionSerializers

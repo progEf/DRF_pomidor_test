59c06b3297e4e39f6d9c7c0487ea551a1f1d6aec
bot_tel <qweasd12401u@gmail.com>
2023-08-19 13:07:35 +0300

    day 2-1

--- service/service/settings.py
+++ service/service/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'clients',
+    'services',
 ]
 
 MIDDLEWARE = [
@@ -82,7 +85,7 @@ WSGI_APPLICATION = 'service.wsgi.application'
 DATABASES ={
     'default':{
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.envrion.get('DB_HOST'),
+        'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
